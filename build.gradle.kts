7bdfffb97e6329533f92a75979e8f9854cf3c2de
Runar Røssevold <126164337+ranuru@users.noreply.github.com>
2025-12-19 14:41:01 +0100

    project files added

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.app"
+version = "0.0.1-SNAPSHOT"
+description = "Polling application"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/app/PollApp/PollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.PollApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

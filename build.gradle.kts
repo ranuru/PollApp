2527ca67559cbc4e0fc26021ec2f0bcf9b81b9ce
Runar Røssevold <126164337+ranuru@users.noreply.github.com>
2026-01-01 20:35:57 +0100

    Update .gitignore, remove unused dependencies, and add controller classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,7 @@ sonar {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
---
+++ src/main/java/com/app/PollApp/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.app.PollApp.controller;
+
+public class UserController {
+    
+}
